2a33f49022764eaaa00f403f6e61ac17b159da67
aaron Francolino <aaron.e.francolino@gmail.com>
2025-02-04 17:06:54 -0300

    correct email

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -25,7 +25,7 @@ app.post('/send-email', (req, res) => {
   let mailOptions = {
     from: email,
     replyTo: email, // // Correo del usuario que llenó el formulario
-    to: 'aaron.e.francolino@gmail.com', // Correo de destino (empresa)
+    to: 'info@salemtech.io', // Correo de destino (empresa)
     subject: asunto,
     html: `
     <html>
